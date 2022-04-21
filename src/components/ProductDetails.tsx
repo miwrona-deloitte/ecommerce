2af7065de0b55d6ee7fdd3a0b18de309fded77e5
Michal Wrona <miwrona@deloittece.com>
2022-04-21 11:40:29 +0200

    ProductDetails js -> ts

---
+++ src/components/ProductDetails.tsx
@@ -0,0 +1,44 @@
+import { getProducts } from "../products";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../store";
+import React from "react";
+
+const ProductDetails: React.FC<{ productId: number }> = (props) => {
+  interface Item {
+    id: number;
+    name: string;
+    price: string;
+    url: string;
+  }
+
+  const dispatch = useDispatch();
+  const products: Item[] = getProducts();
+  let product: Item | undefined = products.find(
+    (product) => product.id === Number(props.productId)
+  );
+  let imgUrl = "/../../pictures/default.jpeg";
+  if (typeof product == "object") {
+    imgUrl = "/../../" + product.url;
+  }
+
+  const addToCartHandler = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    dispatch(cartActions.addToCart({ product: product }));
+  };
+
+  const productName = typeof product == "object" ? product.name : "";
+
+  return (
+    <>
+      <Link to="/catalog">Catalog</Link> | <Link to="/cart">Cart</Link>
+      <div>
+        <span>{productName}</span>
+        <img src={imgUrl} width="400" alt={imgUrl} />
+        <button onClick={addToCartHandler}>Add To Cart</button>
+      </div>
+    </>
+  );
+};
+
+export default ProductDetails;
