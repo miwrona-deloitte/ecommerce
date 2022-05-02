d5b3d54ad74379dd7488ffbe4c8da73d2540e9f7
Michal Wrona <miwrona@deloittece.com>
2022-05-02 13:01:22 +0200

    toggle minicart, basic styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Minicart from "./../Minicart";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [showMinicart, setShowMinicart] = useState(false);
+  const counter = useSelector((state: RootState) => state.cart.counter);
   return (
     <>
-      <nav style={{textAlign: "center"}}>      
-        <Link to="/">Home</Link> |{" "} 
-        <Link to="/cart">Cart</Link> |{" "} 
-        <Link to="/catalog">Catalog</Link>
-      </nav>
-      <Minicart />
-      <main style={{textAlign: "center"}}>{children}</main>
+      <div>
+        <nav style={{ textAlign: "center" }}>
+          <Link to="/">Home</Link> | <Link to="/cart">Cart</Link> |{" "}
+          <Link to="/catalog">Catalog</Link>
+        </nav>
+        <span style={{ position: "absolute", right: "10%", cursor: "pointer" }}>
+          {counter}
+          <img
+            onClick={() => {
+              setShowMinicart(!showMinicart);
+            }}
+            src="/../../../pictures/cart.png"
+            width="30px"
+            alt="cart icon"
+          />
+        </span>
+      </div>
+      {showMinicart && <Minicart />}
+      <main style={{ textAlign: "center" }}>{children}</main>
     </>
   );
 };
--- src/components/Minicart.tsx
+++ src/components/Minicart.tsx
@@ -2,7 +2,16 @@ import Cart from "./Cart";
 
 const Minicart: React.FC = () => {
   return (
-    <div style={{ position: "absolute", right: "100px" }}>
+    <div
+      style={{
+        position: "absolute",
+        right: "12%",
+        top: "52px",
+        border: "solid 1px grey",
+        borderRadius: "10px",
+        padding: "5px"
+      }}
+    >
       <Cart />
     </div>
   );
