75a54b77b468bf6480c484d895ecae4f40e7c6dd
Michal Wrona <miwrona@deloittece.com>
2022-06-20 12:35:43 +0200

    homepage

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,8 +1,29 @@
+import CatalogItem from './CatalogItem';
+import { getProducts } from '../dictionary/products';
+import './Homepage.scss';
+
 const Homepage = () => {
+  const products = getProducts();
   return (
     <>
-      <div>Banner</div>
-      <div>Bestsellers</div>
+      <div className='banner'>
+        <div className='card'>
+          <div className='banner-content'>
+            <h1>Our new collection</h1>
+            <p>A new collection inspired by Scandinavian motifs.</p>
+            <button>Check now!</button>
+          </div>
+        </div>
+        <div className='picture'></div>
+      </div>
+      <div className='bestsellers'>
+        <h2>Bestsellers</h2>
+        <div className='container'>
+          {products.map(product => (
+            <CatalogItem product={product} key={product.id} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
